519e4bb04de0a4591ad0875a39c28bba0e98d091
Tobias Schlottke <tobias.schlottke@gmail.com>
2012-12-27 17:00:46 +0100

    added support for on duplicate

--- lib/activerecord-import/adapters/jdbcmysql_adapter.rb
+++ lib/activerecord-import/adapters/jdbcmysql_adapter.rb
@@ -3,4 +3,10 @@ require "activerecord-import/adapters/mysql_adapter"
 
 class ActiveRecord::ConnectionAdapters::MysqlAdapter
   include ActiveRecord::Import::MysqlAdapter
+  include ActiveRecord::Import::OnDuplicateKeyUpdateSupport
+
+  #return true if the statement is a duplicate key record error
+  def duplicate_key_update_error?(exception)# :nodoc:
+    exception.is_a?(ActiveRecord::JDBCError) && exception.to_s.include?('Duplicate entry')
+  end
 end
